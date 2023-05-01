7dfdca73152c331e96ee58b931a033de360eeb1d
Alex Kim <71356356+ajk3158@users.noreply.github.com>
2023-05-01 13:22:36 -0400

    Remove Phone #

--- frontend_react/src/container/Contact/Contact.jsx
+++ frontend_react/src/container/Contact/Contact.jsx
@@ -20,12 +20,6 @@ const Contact = () => {
         <a href="mailto:alexjoonkim02@gmail.com" className="p-text app__contact-section">Email</a>
       </div>
 
-      {/* Phone Number contact card */}
-      <div className="app__contact-card">
-        <img src={images.mobile} alt="mobile"/>
-        <a href="tel: +1 (703) 380-5515" className="p-text app__contact-section">+1 (703) 380-5515</a>
-      </div>
-
       {/* LinkedIn contact card */}
       <div className="app__contact-card">
         <img src={images.linkedin} alt="LinkedIn"/>
